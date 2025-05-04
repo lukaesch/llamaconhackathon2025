353c6608f66796db929746ee62ebecd34a9a0800
sascha10000 <github@duepre-sa.de>
2025-05-03 17:04:57 -0700

    gitignore

--- llama-graph-rag/target/debug/deps/llama_graph_rag-f741e7e8bcd9c8a2.d
+++
@@ -1,5 +0,0 @@
-/Users/sascha/workspace/projects/llamaconhackathon2025/llama-graph-rag/target/debug/deps/libllama_graph_rag-f741e7e8bcd9c8a2.rmeta: src/main.rs
-
-/Users/sascha/workspace/projects/llamaconhackathon2025/llama-graph-rag/target/debug/deps/llama_graph_rag-f741e7e8bcd9c8a2.d: src/main.rs
-
-src/main.rs:
